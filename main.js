a31b83abe4f38d093e8310c9b021e88114b87901
Jeremiah-Ng <72901442+Jeremiah-Ng@users.noreply.github.com>
2021-05-07 18:16:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function webcam(){
+ document.getElementById("complicated").innerHTML="Webcam View - "
+    document.getElementById("camera").style.height="350px";
+        document.getElementById("camera").style.visibility="visible";
+    document.getElementById("camera1").style.visibility="hidden";
+    document.getElementById("camera1").style.height="0px";
+}
+function files(){
+    document.getElementById("complicated").innerHTML="File Upload - "
+    document.getElementById("camera1").style.visibility="visible";
+        document.getElementById("camera1").style.height="350px";
+        document.getElementById("camera").style.height="0px";
+        document.getElementById("camera").style.visibility="hidden";
+}
